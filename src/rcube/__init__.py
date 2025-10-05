09512d6af4d7c5ec266ba75c6f269cdc5418a710
Paul Knutson <catsymptote@gmail.com>
2025-10-05 17:42:36 +0200

    Added __init__ file

---
+++ src/rcube/__init__.py
@@ -0,0 +1,9 @@
+from rcube import cube_interface, pochmann_solver, scrambler, logger, algorithm
+
+'''
+from rcube.cube_interface import CubeInterface
+from rcube.pochmann_solver import PochmannSolver
+from rcube.scrambler import Scrambler
+from rcube.logger import Logger
+from rcube.algorithm import Algorithm
+'''
